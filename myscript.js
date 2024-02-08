9e8968fc9737d5cc886f179fce125ff24404e5f9
JamisonSavellano <158102826+JamisonSavellano@users.noreply.github.com>
2024-02-08 13:41:30 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
 	});
+
+	$("#p1").click(function(){
+		$("disappear").toggle();
+		});
+	
 	$("h1").click(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
 		$("img").css("border","25px solid black");

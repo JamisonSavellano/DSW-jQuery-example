86e5f2e6e502e8d00af9839fcf11f6375d184a69
JamisonSavellano <158102826+JamisonSavellano@users.noreply.github.com>
2024-02-08 13:46:38 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -6,6 +6,10 @@ $(document).ready(function() {
 	$("#p1").click(function(){
 		$("disappear").toggle();
 		});
+
+	$("#p2").mouseenter(function(){
+		alert("There is a second paragraph here, press paragraph 1 to see it");
+		});
 	
 	$("h1").click(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
